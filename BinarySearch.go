c5770d4740262085c04eba02d37266338579fbd1
eduardoalexandree <eduardoalexandree@github.com>
2023-08-10 22:41:43 -0300

    binary_search

--- BinarySearch.go
+++ BinarySearch.go
@@ -3,7 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	list := []int{14, 15, 16, 17, 18, 19, 20, 21}
+	value := 18
+	if binarySearch(list, value) != nil {
+		fmt.Println("Value found")
+	} else {
+		fmt.Println("Value not found")
+	}
+}
 
-	fmt.Println("teste")
+func binarySearch(l []int, value int) *int {
+	low := 0
+	high := len(l) - 1
+	for low <= high {
+		mean := (low + high) / 2
+		fmt.Println(l[mean])
+		if l[mean] == value {
+			return &l[mean]
+		}
+		if l[mean] > value {
+			high = mean - 1
+		}
+		if l[mean] < value {
+			low = mean + 1
+		}
+	}
+	return nil
 
 }
